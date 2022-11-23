ee3242b0c8d3251c85a392b9825b61ab545cb6a3
Сергей Плюхин <psg.pgs@mail.ru>
2022-11-23 21:59:47 +0300

    Добавьте файлы проекта.

---
+++ HomeWork17/BankCheck.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork17
+{
+    class BankCheck<T>
+    {
+        //Создать класс для моделирования счета в банке.
+        //Предусмотреть закрытые поля для
+        //номера счета,
+        //баланса,
+        //ФИО владельца.
+        //Класс должен быть объявлен как обобщенный.
+        //Универсальный параметр T должен определять тип номера счета.
+        //Разработать методы  для доступа  к данным  –  заполнения и чтения.
+
+        private T numChek;
+        private string name;
+        private decimal check;
+
+        //public void SetName(string value)
+        //{
+        //    name = value;
+        //}
+
+        //public void SetCheck(decimal value) { check = value; }
+        //public string Name
+        //{
+        //    get
+        //    { return name; }
+        //    set
+        //    { name = value; }
+        //}
+
+        public void Input(T numChek, string name,decimal check)
+        {
+            this.numChek = (T)Convert.ChangeType(numChek,typeof(T));
+            this.name = name;
+            this.check = check;
+        }
+
+        public string Info()
+        {
+            return $"{numChek} {name} {check}";
+        }
+    }
+}
---
+++ HomeWork17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создать несколько экземпляров класса, параметризированного различными типам.
+            //Заполнить его поля и вывести на экран информацию об экземпляре класса.
+            BankCheck<int> bankCheck_int=new BankCheck<int>() ;
+            bankCheck_int.Input(1220111, "Иванов Иван Иванович", (decimal)999.99);
+            Console.WriteLine(bankCheck_int.Info());
+
+            BankCheck<string> bankCheck_string = new BankCheck<string>();
+            bankCheck_string.Input("A220AAA", "Иванов Иван Иванович", (decimal)999.99);
+            Console.WriteLine(bankCheck_string.Info());
+
+            Console.ReadKey();
+        }
+    }
+}
